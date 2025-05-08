96237bfaf7a8c267073ece236e984730e5cc4b89
Bagas Wastu <git@wastu.net>
2025-05-08 15:36:01 +0700

    chore: remove unused parts

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -3,7 +3,6 @@ import { cookies } from 'next/headers';
 import { AppSidebar } from '@/components/app-sidebar';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
-import Script from 'next/script';
 
 export const experimental_ppr = true;
 
@@ -17,10 +16,6 @@ export default async function Layout({
 
   return (
     <>
-      <Script
-        src="https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js"
-        strategy="beforeInteractive"
-      />
       <SidebarProvider defaultOpen={!isCollapsed}>
         <AppSidebar />
         <SidebarInset>{children}</SidebarInset>
